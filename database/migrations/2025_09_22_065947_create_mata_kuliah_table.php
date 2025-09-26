e9492b2a9cc94c71c2fdaf7a0cdf33c01f09ba86
Febrianadella <indahdella00@gmail.com>
2025-09-26 08:24:31 +0700

    comit chance mk

---
+++ database/migrations/2025_09_22_065947_create_mata_kuliah_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('mata_kuliah', function (Blueprint $table) {
+            $table->id(); // kolom id (bigint, auto increment)
+            $table->string('nama_mk'); // kolom nama_mk
+            $table->integer('sks'); // kolom sks
+            $table->timestamps(); // kolom created_at & updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mata_kuliah');
+    }
+};
